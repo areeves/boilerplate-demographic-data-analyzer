d6dd1d18c2d3df7cb54b7798c0e47260a517c1fa
Andrew Reeves <areeves42@gmail.com>
2026-03-28 20:32:59 -0400

    percentage_bachelors

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -12,7 +12,7 @@ def calculate_demographic_data(print_data=True):
 
 
     # What is the percentage of people who have a Bachelor's degree?
-    percentage_bachelors = None
+    percentage_bachelors = ( (df['education'] == 'Bachelors').mean() * 100 ).round(1).item()
 
     # What percentage of people with advanced education (`Bachelors`, `Masters`, or `Doctorate`) make more than 50K?
     # What percentage of people without advanced education make more than 50K?
